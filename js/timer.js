45915515048ccc839878a6bc380a6471e7ec50b7
segea17 <segea86@gmail.com>
2023-04-12 16:17:36 -0300

    Hecho Timer. Falta acomodar el media query para que no se rompa

---
+++ js/timer.js
@@ -0,0 +1,14 @@
+var fechaLimite = new Date("April 17, 2023 00:00:00").getTime();
+var x = setInterval(function() {
+    var ahora = new Date().getTime();
+    var tiempoRestante = fechaLimite - ahora;
+    var dias = Math.floor(tiempoRestante/(1000*60*60*24))
+    var horas = Math.floor((tiempoRestante % (1000*60*60*24))/(1000*60*60));
+    var minutos = Math.floor((tiempoRestante % (1000*60*60))/(1000*60));
+    var segundos = Math.floor((tiempoRestante % (1000*60))/(1000));
+document.getElementById("timer").innerHTML= dias + " días " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+if (tiempoRestante<0){
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "La oferta expiró"
+}
+}, 1000);
